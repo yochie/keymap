1afb4b1a46e871cdde22e23b37d7954a550a3af9
yochie <yoann.ochietti@gmail.com>
2024-06-18 02:14:15 -0400

    oled config to display layer info including default layer

--- keymap.c
+++ keymap.c
@@ -98,3 +98,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case BASE:
+            //not sure why this has to be inverted...
+            if (default_layer_state != GAMING){
+                oled_write_P(PSTR("Gaming\n"), false);
+            } else {
+                oled_write_P(PSTR("Base\n"), false);
+            }
+            break;
+//        case GAMING:
+//            oled_write_P(PSTR("Gaming\n"), false);
+//            break;
+        case NUMS_NAV:
+            oled_write_P(PSTR("Nums\n"), false);
+            break;
+        case SYMBOLS:
+            oled_write_P(PSTR("Symbols\n"), false);
+            break;
+        case MEDIA:
+            oled_write_P(PSTR("Media\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+//    led_t led_state = host_keyboard_led_state();
+//    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+//    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+//    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
+#endif
